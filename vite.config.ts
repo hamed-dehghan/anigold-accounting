3254d26beaf88a24a06835c398dc8fad78e755a5
hamed dehghan <hamed137729@gmail.com>
2025-02-24 15:10:29 +0330

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Adjust this if your app is in a subdirectory
   server: {
     open: true, // Automatically open the browser
     hmr: true,  // Hot module replacement
